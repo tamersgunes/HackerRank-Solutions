6bb67c73744e5339f076753b8a4e81592942782e
tamergunes-multibankfx <tamer.gunes@multibankfx.com>
2021-09-12 22:29:00 +0300

    MRS-1 Solve Two String Question

---
+++ src/main/java/com/hackerrank/solutions/controller/TwoStringsController.java
@@ -0,0 +1,22 @@
+package com.hackerrank.solutions.controller;
+
+import com.hackerrank.solutions.service.TwoStringsService;
+import com.hackerrank.solutions.util.ApiConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class TwoStringsController {
+    private final TwoStringsService twoStringsService;
+
+    public TwoStringsController(TwoStringsService twoStringsService) {
+        this.twoStringsService = twoStringsService;
+    }
+
+    @PostMapping(value = ApiConstants.RESOLVE_TWO_STRINGS)
+    public ResponseEntity<String> resolveStrings(String string1, String string2) {
+        return new ResponseEntity<>(twoStringsService.twoStrings(string1, string2), null, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/hackerrank/solutions/service/TwoStringsService.java
@@ -0,0 +1,15 @@
+package com.hackerrank.solutions.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TwoStringsService {
+    public String twoStrings(String s1, String s2) {
+        for (char c : "abcdefghijklmnopqrstuvwxyz".toCharArray()) {
+            if (s1.indexOf(c) > -1 && s2.indexOf(c) > -1) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+}
