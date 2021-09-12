8a88da5f55c1c2f0f8241077497b9010f03a15a2
tamergunes-multibankfx <tamer.gunes@multibankfx.com>
2021-09-12 22:23:23 +0300

    First-Commit

---
+++ src/main/java/com/hackerrank/solutions/SolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.hackerrank.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionsApplication.class, args);
+	}
+
+}
